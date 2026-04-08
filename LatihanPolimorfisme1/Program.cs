471a8aa618408acf0c6f36a524701e2a6eca487f
joharadinata10-byte <joharadinata10@gmail.com>
2026-04-08 14:46:57 +0700

    Latihan1

--- LatihanPolimorfisme1/Program.cs
+++ LatihanPolimorfisme1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Imageprocessor imageprocessor = new Imageprocessor();
+
+imageprocessor.Resize(-20);
+imageprocessor.Resize(80);
+imageprocessor.Resize(150);
+imageprocessor.Resize(100);
+imageprocessor.Resize(10, 15);
+imageprocessor.Resize(300);
+
+
+class Imageprocessor
+{
+    public void Resize(int persentase)
+    {
+        if (persentase <= 0)
+        {
+            Console.WriteLine("Error! Persentase tidak boleh nol atau negatif");
+        }
+        else if (persentase < 100)
+        {
+            Console.WriteLine($"Mengecilkan gambar menjadi {persentase}%");
+        }
+        else if (persentase > 100)
+        {
+            Console.WriteLine($"Membesarkan gambar menjadi {persentase}%");
+        }
+        else
+        {
+            Console.WriteLine("Ukuran tetap (100%). Tidak ada perubahan");
+        }
+    }
+
+    public void Resize(int lebar, int tinggi)
+    {
+        Console.WriteLine($"Mengubah ukuran gambar menjadi {lebar}px X {tinggi}px");
+    }
+    public void Resize(string ukuran_kertas)
+    {
+        Console.WriteLine($"Menyesuaikan gambar menjadi {ukuran_kertas}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
